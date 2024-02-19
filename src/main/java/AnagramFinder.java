94db18c94e081488757216feeedb2327686ae6f9
hans4dev <16078890+hans4dev@users.noreply.github.com>
2024-02-19 20:59:53 +0100

    feat: verify single word anagrams correctly

--- src/main/java/AnagramFinder.java
+++ src/main/java/AnagramFinder.java
@@ -1,3 +1,13 @@
+import java.util.Arrays;
+
+import static java.util.Arrays.*;
+
+/**
+ * Uses the approach "sort char-arrays" to verify anagrams.
+ * <p>
+ * A predicate method {{@link #areAnagrams(String, String)} can be used to verify anagrams.
+ * </p>
+ */
 public class AnagramFinder {
     /**
      * Verifies if two given texts are anagrams of each other.
@@ -7,6 +17,12 @@ public class AnagramFinder {
      * @return true if phrase is an anagram of phrase
      */
     public static boolean areAnagrams(String subject, String phrase) {
-        return false;
+
+        char[] subjectChars = subject.toCharArray();
+        sort(subjectChars);
+        char[] phraseChars = phrase.toCharArray();
+        sort(phraseChars);
+
+        return Arrays.equals(subjectChars, phraseChars);
     }
 }
