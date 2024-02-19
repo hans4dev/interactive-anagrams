efe3b9d7979da065dbb800ccb6e5597f466e3763
hans4dev <16078890+hans4dev@users.noreply.github.com>
2024-02-19 22:14:15 +0100

    refactor: anagrams require at least 2 letters

--- src/main/java/AnagramFinder.java
+++ src/main/java/AnagramFinder.java
@@ -4,7 +4,7 @@ import static java.util.Arrays.*;
 
 /**
  * Uses the approach "sort character arrays" to verify anagrams.
- * Considers letters (a-z) only, letter-case, whitespace and punctuation or other characters are ignored.
+ * Considers letters (a-z) only while letter-case, whitespace and punctuation or other characters are ignored.
  * <p>
  * A predicate method {{@link #areAnagrams(String, String)} can be used to verify anagrams.
  * </p>
@@ -16,29 +16,38 @@ public class AnagramFinder {
      * @param subject a word or phrase
      * @param phrase  another word or phrase
      * @return true if phrase is an anagram of subject, false otherwise
-     * @throws IllegalArgumentException if any argument is null, blank or trimmed less than 2 letters
+     * @throws IllegalArgumentException if any argument is null, blank or cleaned less than 2 letters
      */
     public static boolean areAnagrams(String subject, String phrase) throws IllegalArgumentException {
 
-        requireMinLength(subject, 2);
-        requireMinLength(phrase, 2);
+        char[] subjectLetters = requireLetters(subject, 2).toCharArray();
+        char[] phraseLetters = requireLetters(phrase, 2).toCharArray();
 
-        char[] subjectLetters = letters(subject);
+        if (subjectLetters.length != phraseLetters.length) {
+            return false;
+        }
+
         sort(subjectLetters);
-        char[] phraseLetters = letters(phrase);
         sort(phraseLetters);
 
         return Arrays.equals(subjectLetters, phraseLetters);
     }
 
-    private static char[] letters(String text) {
-        return text.trim().toLowerCase().replaceAll("[^a-z]+", "").toCharArray();
+    private static String letters(String text) {
+        return text.trim().toLowerCase().replaceAll("[^a-z]+", "");
     }
 
-    private static void requireMinLength(String text, int minimumLength) {
+    private static String requireLetters(String text, int minimumLength) {
         if (text == null || text.isBlank() || text.trim().length() < minimumLength) {
             throw new IllegalArgumentException(String.format("must be not null and have a minimum length of %d", minimumLength));
         }
+
+        String letters = letters(text);
+        if (letters.length() < minimumLength) {
+            throw new IllegalArgumentException(String.format("must contain at least %d letters", minimumLength));
+        }
+
+        return letters;
     }
 
 }
--- src/test/groovy/AnagramFinderTest.groovy
+++ src/test/groovy/AnagramFinderTest.groovy
@@ -40,6 +40,20 @@ class AnagramFinderTest extends Specification {
         "null"   | null
     }
 
+    def "requires at least 2 letters (after cleaning)"(String text1, String text2) {
+        when: "minimal requirements not met"
+        AnagramFinder.areAnagrams(text1, text2)
+
+        then: "throws an illegal argument exception with corrective help message"
+        def exception = thrown(IllegalArgumentException)
+        exception.message == 'must contain at least 2 letters'
+
+        where: "two texts where one has less than 2 letters"
+        text1 | text2
+        "She" | "S__"
+        "She" | "S__ ?!"
+    }
+
     def "true for sentences that are anagrams (ignore letter-case, whitespace and punctuation)"(String text1, String text2) {
         expect: "true is returned when passed to predicate method"
         AnagramFinder.areAnagrams(text1, text2)
@@ -49,6 +63,17 @@ class AnagramFinderTest extends Specification {
         "funeral"             | "real fun"
         "New York Times"      | "monkeys write"
         "She Sells Sanctuary" | "Santa; shy, less cruel"
+    }
+
+    def "false for sentences that are no anagrams (letter length varies)"(String text1, String text2) {
+        expect: "false is returned when passed to predicate method"
+        !AnagramFinder.areAnagrams(text1, text2)
+
+        where: "two texts which are anagrams"
+        text1                      | text2
+        "funeral"                  | "fun"
+        "New York Times is longer" | "monkeys write"
+        "She Sells Sanctuary"      | "The Cult"
     }
 
 }
