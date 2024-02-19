1a78369e05cb7cae9d68c2d4a7f4f6653bdc0358
hans4dev <16078890+hans4dev@users.noreply.github.com>
2024-02-19 21:43:44 +0100

    test: allow sentences (ignore case, space and punctuation)

--- src/test/groovy/AnagramFinderTest.groovy
+++ src/test/groovy/AnagramFinderTest.groovy
@@ -39,4 +39,16 @@ class AnagramFinderTest extends Specification {
         "null"   | null
         "null"   | null
     }
+
+    def "true for sentences that are anagrams (ignore letter-case, whitespace and punctuation)"(String text1, String text2) {
+        expect: "true is returned when passed to predicate method"
+        AnagramFinder.areAnagrams(text1, text2)
+
+        where: "two texts which are anagrams"
+        text1                 | text2
+        "funeral"             | "real fun"
+        "New York Times"      | "monkeys write"
+        "She Sells Sanctuary" | "Santa; shy, less cruel"
+    }
+
 }
